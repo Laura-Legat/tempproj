7701f3b016c154c25c5dbfbcf2ec5ca7647cda31
Laura Legat <laura.legat11@gmail.com>
2025-10-08 01:17:07 +0200

    fix date issue

--- api/reason.js
+++ api/reason.js
@@ -1,18 +1,15 @@
 export default function handler(req, res) {
     const raw = process.env.REASONS || "";
     const reasons = raw.split("|");
-    console.log(reasons)
 
     const startDate = new Date("2025-10-08T00:00:00Z");
     const now = new Date();
 
     const msPerDay = 24 * 60 * 60 * 1000;
-    const daysSinceStart = Math.floor((now - startDate) / msPerDay);
+    const daysSinceStart = Math.max(0, Math.floor((now - startDate) / msPerDay));
 
     const index = daysSinceStart % reasons.length;
-    console.log(index)
     const reason = reasons[index];
-    console.log(reason)
 
     res.status(200).json({ reason });
 }
